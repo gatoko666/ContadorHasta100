ae02139b04ad5556b59a0b2621f67ea5c1983fa6
gatoko <g.millaquiensuarez@gmail.com>
2018-03-01 01:24:45 -0300

    Ejercicio terminado

---
+++ ContadorHasta100/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Utilizando un iterador escribir en pantalla los números del 1 al 100 de dos en dos.
+
+namespace ContadorHasta100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 1;
+                
+                    for (int i = 1; i<=50 ;i++)
+                    {
+                        
+                        Console.WriteLine(contador);
+                        contador++;
+                        contador++;
+          }
+                    Console.Read();               
+            }
+        }
+    }
+
+   
+
